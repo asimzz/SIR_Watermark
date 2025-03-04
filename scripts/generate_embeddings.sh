d63b9e6c19ecbf838eaeb53d549259b1e6f21af8
Asim Abdalla <asimabdalla99@gmail.com>
2025-03-04 18:18:59 +0000

    update embedding model path in generate_embeddings script

--- scripts/generate_embeddings.sh
+++ scripts/generate_embeddings.sh
@@ -4,14 +4,14 @@ set -u # Treat unset variables as an error when substituting.
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 WORK_DIR=$SCRIPT_DIR/..
 DATA_DIR=$WORK_DIR/data
-EMBEDDING_MODEL_DIR=$WORK_DIR/model
+EMBEDDING_MODEL=xxx/compositional-bert-large-uncased
 
 echo "Generating embeddings for training data..."
 
 python3 $WORK_DIR/generate_embeddings.py \
     --input_path $DATA_DIR/sts/train.jsonl \
     --output_path $DATA_DIR/embeddings/train_embeddings.txt \
-    --model_path $EMBEDDING_MODEL_DIR/transform_model_cbert.pth \
+    --model_path $EMBEDDING_MODEL
     --size 4000
 
 echo "Generating embeddings for validation data..."
@@ -19,7 +19,7 @@ echo "Generating embeddings for validation data..."
 python3 $WORK_DIR/generate_embeddings.py \
     --input_path $DATA_DIR/sts/validation.jsonl \
     --output_path $DATA_DIR/embeddings/validation_embeddings.txt \
-    --model_path $EMBEDDING_MODEL_DIR/transform_model_cbert.pth \
+    --model_path $EMBEDDING_MODEL
     --size 2000
 
 echo "Generating embeddings for test data..."
@@ -27,5 +27,5 @@ echo "Generating embeddings for test data..."
 python3 $WORK_DIR/generate_embeddings.py \
     --input_path $DATA_DIR/sts/test.jsonl \
     --output_path $DATA_DIR/embeddings/test_embeddings.txt \
-    --model_path $EMBEDDING_MODEL_DIR/transform_model_cbert.pth \
+    --model_path $EMBEDDING_MODEL
     --size 1000
